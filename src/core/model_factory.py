1b5af8cd62eba4a9f772c8073b826374a212c5ed
cheryllfid-s <e1075554@u.nus.edu>
2025-04-14 22:30:53 +0800

    Update model_factory.py
    added randomforest

--- src/core/model_factory.py
+++ src/core/model_factory.py
@@ -2,13 +2,13 @@
 
 from models.svm import SVMModel
 # from models.logistic import LogisticModel
-# from models.rf import RandomForestModel
+from models.rf import RandomForestModel
 
 def get_model(model_type, **kwargs):
     model_map = {
         "svm": SVMModel,
         # "logistic": LogisticModel,
-        # "rf": RandomForestModel,
+        "rf": RandomForestModel,
     }
     if model_type not in model_map:
         raise ValueError(f"Unknown model: {model_type}")
