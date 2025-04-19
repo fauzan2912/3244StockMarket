c23fe001a9acde1b39d4f3ebbb608d4ab4fbbea4
fauzan2912 <fauzan2912@gmail.com>
2025-04-19 22:11:20 +0800

    updated logistic regression tuning and results

--- src/tuning/logistic.py
+++ src/tuning/logistic.py
@@ -14,11 +14,12 @@ def tune_logistic(X_train, y_train, X_val, y_val, val_returns):
     X_train_scaled = scaler.fit_transform(X_train)
     X_val_scaled = scaler.transform(X_val)
 
-    param_grid = {
-        'C': [0.1, 1, 10],
-        'penalty': ['l2', 'l1', 'elasticnet'],
-        'solver': ['lbfgs', 'liblinear', 'newton-cholesky', 'sag', 'saga']
-    }
+    param_grid = [
+        {'penalty': ['l2'], 'C': [0.1, 1, 10], 'solver': ['lbfgs', 'sag', 'newton-cholesky']},
+        {'penalty': ['l1', 'l2'], 'C': [0.1, 1, 10], 'solver': ['liblinear']},
+        {'penalty': ['l1', 'l2'], 'C': [0.1, 1, 10], 'solver': ['saga']},
+        {'penalty': ['elasticnet'], 'C': [0.1, 1, 10], 'solver': ['saga'], 'l1_ratio': [0.5, 0.7, 0.9]}
+    ]
 
     search = RandomizedSearchCV(
         estimator=LogisticRegression(),
@@ -32,7 +33,12 @@ def tune_logistic(X_train, y_train, X_val, y_val, val_returns):
     search.fit(X_train_scaled, y_train)
     best_params = search.best_params_
 
-    best_model = LogisticRegression(**best_params)
+    # Filter out irrelevant params (like l1_ratio for non-elasticnet)
+    filtered_params = {
+        k: v for k, v in best_params.items() if k in LogisticRegression().get_params()
+    }
+
+    best_model = LogisticRegression(**filtered_params)
     best_model.fit(X_train_scaled, y_train)
 
     y_pred = best_model.predict(X_val_scaled)
